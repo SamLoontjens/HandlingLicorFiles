5bdaa8c672e09de8f71499a462788f47722bd20b
SamLoontjens <93867680+SamLoontjens@users.noreply.github.com>
2021-11-09 10:42:19 +0100

    Changing stuff

--- R/say_hi.R
+++ R/say_hi.R
@@ -1,5 +1,7 @@
 #' A function to say hi to Sam
+#'
 #' @description A function that say hi to Sam!
+#'
 #' @export
 say_hi <- function() {
   print("Say hi to Sam Loontjens and give him a cookie")
---
+++ R/testfunction.R
@@ -0,0 +1,8 @@
+#' A function for testing
+#'
+#' @description A function to use in a different function worked!
+#'
+#' @export
+testfunction <- function() {
+  print("A function to use in a different function worked!")
+}
